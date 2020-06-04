a87a23904a3bba2cd93d71eb4dc22daf56b2c778
Ornélio Chaúque <chauque.ornelio@gmail.com>
2020-06-04 16:03:54 +0200

    teste upload file

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express')
+const  multer= require('multer');
+let form= multer({dest:'./public'});
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
+  .use(express.static(path.resolve(__dirname, 'public')))
+  .set('views', path.resolve(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+
+  .post('prescription/add', form.single('photo'), (req, res)=>{
+    console.log(req.file);
+    res.json({message:"ok"});
+  })
+
+  .get("/prescription", (req, res)=>{
+    res.send("Send reposnse");
+  })
+
+
+
+
+
+
+
+
+
+
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
