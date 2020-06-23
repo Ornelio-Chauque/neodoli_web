688f57cee50c8f389380f3db8a6e039e6529a5ee
Ornélio Chaúque <chauque.ornelio@gmail.com>
2020-06-23 12:51:41 +0200

    Prescription get test

--- index.js
+++ index.js
@@ -10,15 +10,26 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
 
-  .post('/prescription/add', form.single('photo'), (req, res)=>{
+  .post('/prescription/', form.single('photo'), (req, res)=>{
     console.log(req.file);
     console.log(req.body.address);
     console.log(req.body.contact);
     res.json({message:"ok"});
   })
 
-  .get("/prescription", (req, res)=>{
+  .get("/prescription/{id}", (req, res)=>{
+    console.log('Prescription request id '+req.params.id);
     res.send("Send reposnse");
+  })
+
+  .get("/prescription", (req, res)=>{
+    let Json='[{"date":"23, junho, 2020", "id":"ducbdu859dnnx"}, {"date":"10, Maio, 2020", "id":"jd87ennd4ff"},{"date":"25, junho, 2020", "id":"7shydjkd90984"}]';
+    res.status(200).json(Json);
+  })
+  .get("/prescription/{id}/respose", (req, res)=>{
+
+    console.log('Prescription response request id '+req.params.id);
+    
   })
   
 
